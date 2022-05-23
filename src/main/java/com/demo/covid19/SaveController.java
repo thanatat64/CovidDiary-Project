536cd48b6d3d13bbd874d7643500e089ac11dd7c
nichapat <64010267@kmitl.ac.th>
2022-05-23 12:22:45 +0700

    Add cloes window when click

---
+++ src/main/java/com/demo/covid19/SaveController.java
@@ -0,0 +1,16 @@
+package com.demo.covid19;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class SaveController {
+
+    public void OkButton(ActionEvent event){
+
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+
+    }
+}
